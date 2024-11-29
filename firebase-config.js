23d6f326031d516bb9d688a8e69e07bb82240c0b
msonnen155 <msonnen638@gmail.com>
2024-11-29 20:26:48 +0000

    oi

--- firebase-config.js
+++ firebase-config.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+console.log('Firebase App:', app);
+console.log('Firebase Analytics:', analytics);
